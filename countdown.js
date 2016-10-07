8adc8dfbe4dedf4af648b39d237e53b1705e2c68
Nat York <natyork@gmail.com>
2016-10-07 00:26:16 +0000

    added completed countdown.js

---
+++ countdown.js
@@ -0,0 +1,22 @@
+var countdownGenerator = function(x) {
+  return function() {
+    if(x > 0) {
+      console.log("T-minus " + x + "...");
+    }
+    if(x === 0) {
+      console.log("Blast Off!")
+    }
+    if(x < 0) {
+      console.log("Rockets already gone, bub!")
+    }
+    x--;
+  }
+};
+
+var countdown = countdownGenerator(3);
+countdown(); // T-minus 3...
+countdown(); // T-minus 2...
+countdown(); // T-minus 1...
+countdown(); // Blast Off!
+countdown(); // Rockets already gone, bub!
+countdown(); // Rockets already gone, bub!
---
+++ loadedDie-refactor.js
@@ -0,0 +1,34 @@
+//refactor of loadedDie without using IFFE pattern just to make sure I understand
+//how each function is being invoked.
+
+function loadedDie() {
+  var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
+  var i =0;
+  return function() {
+     var result = list[i];
+     i++;
+     return result;
+  }
+};
+
+var output = loadedDie();
+console.log(output());
+console.log(output());
+console.log(output());
+console.log(output());
+console.log(output());
+
+// could also go one step further and try to create a function that creates
+// custom lists to pass to loadedDie....stretch exercise.
+
+
+
+
+
+
+
+
+
+
+
+
